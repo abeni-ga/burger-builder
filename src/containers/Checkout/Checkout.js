82a49884ae538d9034e8641d38f59e98f611b7f8
Abenezer <agashaye18@gnail.com>
2020-10-10 16:17:37 +0300

    up

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react' ;
+
+import Burger from '../../components/Burger/Burger';
+import Button from '../../components/UI/Button/Button';
+
+class Checkout extends Component {
+
+    render(){
+      
+    }
+}
